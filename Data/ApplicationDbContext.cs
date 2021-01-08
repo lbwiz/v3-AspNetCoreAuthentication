38d8570199f71dc6e17ac57d099b20cd3c022cfe
Mohamad Lawand <mohamad.lawand@outlook.com>
2021-01-08 00:31:51 +0000

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SampleWebAuth.Models;
+
+namespace SampleWebAuth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AdvanceUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
